064d92bbec0309a88a7b31055e02206ec7c51cdd
Cameron Smith <cam@Cam-3.local>
2026-08-05 13:18:32 +1200

    edits to mainloop

---
+++ MainLoop_SetUp/mainLoop.cpp
@@ -0,0 +1,9 @@
+void setup() {
+    Serial.begin(115200);
+    initialiseTokenArray();
+
+}
+
+void loop() {
+    
+}
---
+++ MainLoop_SetUp/mainLoop.h
@@ -0,0 +1,15 @@
+#ifndef MAINLOOP_H
+#define MAINLOOP_H
+
+/* INCLUDE ALL HEADERS FOR ALL FILES */
+#include <Arduino.h>
+
+#include "parse.h"
+#include "GcodeToken.h"
+#include "helperFunctions.h"
+#include "motion.h"
+#include "limitSwitchDebounce.h"
+#include "limitSwitch.h"
+
+
+#endif
