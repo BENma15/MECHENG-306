5fe320d91599609ce657082ba24ae9fb73cc37a7
cbea907 <cbea907@aucklanduni.ac.nz>
2026-08-09 00:51:02 +0000

    created graphing file

---
+++ lib/Graphing/Graph.cpp
@@ -0,0 +1,11 @@
+#include "Graph.h"
+#include <vector>
+
+std::vector<long> data;
+std::vector<long> time;
+
+void addDataPoint(long dataPoint, long time) {
+    data.push_back(dataPoint);
+    time.push_back(timePoint);
+}
+
---
+++ lib/Graphing/Graph.h
@@ -0,0 +1,10 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+#include <vector>
+
+extern std::vector<long> data;
+extern std::vector<long> time;
+
+void addDataPoint(long dataPoint, long timePoint);
+
+#endif
