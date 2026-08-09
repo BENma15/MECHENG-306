2a44f438f1b366f24937a2bf7fc5c5c46fdf3515
Cole Beasley <cole@wifi-guest-172-24-110-158.net.auckland.ac.nz>
2026-08-09 13:45:42 +1200

    created the csv maker

---
+++ lib/Graphing/createCSV.py
@@ -0,0 +1,22 @@
+import serial
+
+ser = serial.Serial("COM5", 115200)
+
+recording = False
+
+with open("motor_data.csv", "w") as file:
+
+    while True:
+        line = ser.readline().decode().strip()
+
+        if line == "START_DATA":
+            recording = True
+            print("Receiving data...")
+            continue
+
+        if line == "END_DATA":
+            print("Finished. Saved to motor_data.csv")
+            break
+
+        if recording:
+            file.write(line + "\n")
