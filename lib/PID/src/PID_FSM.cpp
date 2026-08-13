785110f1e00b1914468efb240d77db86aff4a148
BENma15 <benma0215@gmail.com>
2026-08-14 08:10:52 +1200

    Commented code and potentially fixed sync PID

--- lib/PID/src/PID_FSM.cpp
+++ lib/PID/src/PID_FSM.cpp
@@ -8,196 +8,224 @@
 #include <HelperFunctions.h>
 
 
-// Distance to encoder count equation variables
-
+// Time Constant Variables
 double T1 = 0.05;
 double T2 = 0.10;
 double TA = T1 + T2 + T1;
 
+// Movement Variables
 double moveJ = 0;
 double moveVf = 0;
 double moveT4 = 0;
 double moveUnitX = 0;
 double moveUnitY = 0;
 double moveStartTime = 0;
+
 bool moveActive = false;
+bool moveStarted = false;
 
 bool triangleProfile = false;
 
+// Left Motor PID Variables
 double kp_left = 0.1, ki_left = 0.1, kd_left = 0.1;
 double integral_left = 0, lastError_left = 0;
 
+// Right Motor PID Variables
 double kp_right = 0.1, ki_right = 0.1, kd_right = 0.1;
 double integral_right = 0, lastError_right = 0;
 
+// Sync PID Variables
 double kp_sync = 0, ki_sync = 0, kd_sync = 0;
 double integral_sync = 0, lastError_sync = 0;
 
+// Time Control Variables
 uint32_t lastControlLoopMicros = 0;
-const uint32_t CONTROL_LOOP_INTERVAL_US = 20000; // 50Hz
-const double dt = CONTROL_LOOP_INTERVAL_US / 1000000.0; // seconds per control loop tick
+const uint32_t CONTROL_LOOP_INTERVAL_US = 20000;            // 50Hz
+const double dt = CONTROL_LOOP_INTERVAL_US / 1000000.0;     // Seconds per control loop tick
 
-bool moveStarted = false;
+// Motor Direction Variables
+int8_t leftDir;
+int8_t rightDir;
 
+
+// Returns the target velocity at different stages in the movement, using an s-curve profile.
 double velocityProfile_FSM(double J, double Vf, double t4, double t) {
-    double V1 = 0.5 * J * T1 * T1;
-    double V2 = J * T1 * T2;
+    double V1 = 0.5 * J * T1 * T1;          // Velocity after stage 1
+    double V2 = J * T1 * T2;                // Velocity after stage 2
 
-    if (t < T1) {
+    if (t < T1) {                                                       // Stage 1
         return 0.5 * J * t * t;
-    } else if (t < T1 + T2) {
+    } else if (t < T1 + T2) {                                           // Stage 2
         double tau = t - T1;
         return V1 + (J * T1) * tau;
-    } else if (t < TA) {
+    } else if (t < TA) {                                                // Stage 3
         double tau = t - (T1 + T2);
         return V1 + V2 + (J * T1) * tau - 0.5 * J * tau * tau;
-    } else if (t < TA + t4) {
+    } else if (t < TA + t4) {                                           // Stage 4
         return Vf;
-    } else if (t < TA + t4 + T1) {
+    } else if (t < TA + t4 + T1) {                                      // Stage 5
         double tau = t - (TA + t4);
         return Vf - 0.5 * J * tau * tau;
-    } else if (t < TA + t4 + T1 + T2) {
+    } else if (t < TA + t4 + T1 + T2) {                                 // Stage 6
         double tau = t - (TA + t4 + T1);
         return Vf - V1 - (J * T1) * tau;
-    } else if (t < TA + t4 + TA) {
+    } else if (t < TA + t4 + TA) {                                      // Stage 7
         double tau = t - (TA + t4 + T1 + T2);
         return Vf - V1 - V2 - (J * T1) * tau + 0.5 * J * tau * tau;
     } else {
-        // move finished
+        // Move finished
         return 0.0;
     }
 }
 
+// Initial speed plan to find distance, time at constant velocity and if it is a triangular profile.
 void plan_FSM(double x, double y, double vf_target) {
+    // Find hypotenuse
     double S = sqrt(x * x + y * y);
     if (S <= 0.0) return;
 
+    // Both the movement vectors
     moveUnitX = x / S;
     moveUnitY = y / S;
+
+    // Target cruise velocity
     moveVf = vf_target;
+
+    // Jerk required
     moveJ = moveVf / (T1 * (T1 + T2));
 
+    // Distance covered during the acceleration ramp
     double rampDistance = (4.0 / 6.0) * moveJ * T1 * T1 * T1 + moveJ * T1 * T2 * T2;
+
+    // Time spent at constant cruise speed
     double t4 = (S - rampDistance) / moveVf;
 
-    if (t4 < 0) {
+    // If time at cruise speed is negative then implement triangle profile
+    /*if (t4 < 0) {
         triangleProfile = true;
         moveActive = true;
         return;
-    }
+    }*/
 
+    // Start move time timer
     moveT4 = t4;
     moveStartTime = millis() / 1000.0;
-
     moveActive = true;
     return;
 }
 
+// Main PID loop.
 void move_FSM(int x, int y, int vf) {
-if (!moveStarted) {
-    moveStarted = true;
+    do {
+        // If movement has not started then set all the errors to 0 and call the movement plan
+        if (!moveStarted) {
+            moveStarted = true;
 
-    integral_left = 0;
-    lastError_left = 0;
+            integral_left = 0;
+            lastError_left = 0;
 
-    integral_right = 0;
-    lastError_right = 0;
+            integral_right = 0;
+            lastError_right = 0;
 
-    integral_sync = 0;
-    lastError_sync = 0;
+            integral_sync = 0;
+            lastError_sync = 0;
 
-    plan_FSM(x, y, vf);
+            plan_FSM(x, y, vf);
 
-    lastControlLoopMicros = micros();
-}
+            lastControlLoopMicros = micros();
+        }
+        
+        // Ensures there is a constant frequency of 50Hz which we are using for the PID loop as to not
+        // call the motors too frequently.
+        uint32_t nowMicros = micros();
+        if (nowMicros - lastControlLoopMicros < CONTROL_LOOP_INTERVAL_US) {
+            continue;
+        }
+        lastControlLoopMicros = nowMicros;
 
-    uint32_t nowMicros = micros();
-    if (nowMicros - lastControlLoopMicros < CONTROL_LOOP_INTERVAL_US) {
-        return;
-    }
-    lastControlLoopMicros = nowMicros;
+        // Gets current time in reference to start time and then finds target velocity
+        double t = (millis() / 1000.0) - moveStartTime;
+        double V = velocityProfile_FSM(moveJ, moveVf, moveT4, t);
 
-    if (!moveActive) {
-        return;
-    }
+        // This currently stops the motors once the time is finsihed, I think we should change this
+        // to when the distance is met and not time.
+        if (t >= TA + moveT4 + TA) {
+            moveActive = false;
+            moveStarted = false;
 
-    double t = (millis() / 1000.0) - moveStartTime;
-    double V = velocityProfile_FSM(moveJ, moveVf, moveT4, t);
+            setLeftMotor(0, 0);
+            setRightMotor(0, 0);
 
-   if (t >= TA + moveT4 + TA) {
-    moveActive = false;
-    moveStarted = false;
+            return;
+        }
 
-    setLeftMotor(0, 0);
-    setRightMotor(0, 0);
+        // Splits target velocity into left and right motor target velocities using unit vectors
+        double targetLeft  = V * (moveUnitX + moveUnitY);
+        double targetRight = V * (moveUnitX - moveUnitY);
 
-    return;
-}
+        // NEED TO ADD ANTI-WINDUP FOR ALL OF THEM
 
-    double targetLeft  = V * (moveUnitX + moveUnitY);
-    double targetRight = V * (moveUnitX - moveUnitY);
+        // left velocity PID
+        double error_left = targetLeft - velocity_left;
+        integral_left += error_left * dt;
+        double derivative_left = (error_left - lastError_left) / dt;
+        double outputLeft = kp_left * error_left + ki_left * integral_left + kd_left * derivative_left;
+        lastError_left = error_left;
 
-    // left velocity PID
-    double error_left = targetLeft - velocity_left;
-    integral_left += error_left * dt;
-    double derivative_left = (error_left - lastError_left) / dt;
-    double outputLeft = kp_left * error_left + ki_left * integral_left + kd_left * derivative_left;
-    lastError_left = error_left;
+        // right velocity PID
+        double error_right = targetRight - velocity_right;
+        integral_right += error_right * dt;
+        double derivative_right = (error_right - lastError_right) / dt;
+        double outputRight = kp_right * error_right + ki_right * integral_right + kd_right * derivative_right;
+        lastError_right = error_right;
 
-    // right velocity PID
-    double error_right = targetRight - velocity_right;
-    integral_right += error_right * dt;
-    double derivative_right = (error_right - lastError_right) / dt;
-    double outputRight = kp_right * error_right + ki_right * integral_right + kd_right * derivative_right;
-    lastError_right = error_right;
+        // sync PID
+        double ratioLeft = (targetLeft != 0.0) ? (velocity_left / targetLeft)  : 1.0;
+        double ratioRight = (targetRight != 0.0) ? (velocity_right / targetRight) : 1.0;
+        double percentError = (ratioLeft - ratioRight) * 100.0;
+        integral_sync += percentError * dt;
+        double derivative_sync = (percentError - lastError_sync) / dt;
+        double syncCorrection = kp_sync * percentError + ki_sync * integral_sync + kd_sync * derivative_sync;
+        lastError_sync = percentError;
 
-    // sync PID
-    double percentError = 0.0;
-    if (velocity_left != 0.0) {
-        percentError = (velocity_left - velocity_right) / velocity_left * 100.0;
-    }
-    integral_sync += percentError * dt;
-    double derivative_sync = (percentError - lastError_sync) / dt;
-    double syncCorrection = kp_sync * percentError + ki_sync * integral_sync + kd_sync * derivative_sync;
-    lastError_sync = percentError;
+        double finalOutputLeft = outputLeft - syncCorrection;
+        double finalOutputRight = outputRight + syncCorrection;
 
-    double finalOutputLeft  = outputLeft - syncCorrection;
-double finalOutputRight = outputRight + syncCorrection;
+        // Finds the direction of the motors
+        if (finalOutputLeft > 0) {
+            leftDir = 1;
+        }
+        else if (finalOutputLeft < 0) {
+            leftDir = -1;
+        }
+        else {
+            leftDir = 0;
+        }
 
-int8_t leftDir;
-int8_t rightDir;
+        if (finalOutputRight > 0) {
+            rightDir = 1;
+        }
+        else if (finalOutputRight < 0) {
+            rightDir = -1;
+        }
+        else {
+            rightDir = 0;
+        }
 
-if (finalOutputLeft > 0) {
-    leftDir = 1;
-}
-else if (finalOutputLeft < 0) {
-    leftDir = -1;
-}
-else {
-    leftDir = 0;
-}
+        // Sets the desired pwm of the motors
+        int leftPWM = abs((int)finalOutputLeft);
+        int rightPWM = abs((int)finalOutputRight);
 
-if (finalOutputRight > 0) {
-    rightDir = 1;
-}
-else if (finalOutputRight < 0) {
-    rightDir = -1;
-}
-else {
-    rightDir = 0;
-}
+        if (leftPWM > 255) {
+            leftPWM = 255;
+        }
 
-int leftPWM = abs((int)finalOutputLeft);
-int rightPWM = abs((int)finalOutputRight);
+        if (rightPWM > 255) {
+            rightPWM = 255;
+        }
 
-if (leftPWM > 255) {
-    leftPWM = 255;
-}
-
-if (rightPWM > 255) {
-    rightPWM = 255;
-}
-
-String leftSuccess = setLeftMotor(leftDir, leftPWM);
-String rightSuccess = setRightMotor(rightDir, rightPWM);
+        // Motor movement
+        String leftSuccess = setLeftMotor(leftDir, leftPWM);
+        String rightSuccess = setRightMotor(rightDir, rightPWM);
+    } while (moveActive);
 }
