8d263b564b2d46d63c1de6813683293aa6392a7f
Cole Beasley <cbea907@UoA.auckland.ac.nz>
2026-08-19 13:27:49 +1200

    update

--- lib/HelperFunctions/src/HelperFunctions.cpp
+++ lib/HelperFunctions/src/HelperFunctions.cpp
@@ -2,7 +2,7 @@
 
 // Distance to encoder count equation variables
 const double COUNTS_PER_REV = 8256.0;
-const double WHEEL_RADIUS_MM = 7;
+const double WHEEL_RADIUS_MM = 6.75;
 
 long distanceToCounts(double distance_mm) {
     return (long) round(COUNTS_PER_REV * distance_mm / (2.0 * PI * WHEEL_RADIUS_MM));
