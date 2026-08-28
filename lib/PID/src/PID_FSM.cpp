1e6b96513107a59718943a826447afae28c18889
Cole Beasley <cbea907@UoA.auckland.ac.nz>
2026-08-28 15:45:12 +1200

    changed pid variables

--- lib/PID/src/PID_FSM.cpp
+++ lib/PID/src/PID_FSM.cpp
@@ -37,11 +37,11 @@ bool moveStarted = false;
 bool triangleProfile = false;
 
 // Left Motor PID Variables
-double kp_left = 10, ki_left = 3, /**/ kd_left = 0, kff_left = 5.25; // Revert kff to 5.5 if doesnt work
+double kp_left = 10, ki_left = 2, /**/ kd_left = 0, kff_left = 5; // Revert kff to 5.5 if doesnt work
 double integral_left = 0, lastError_left = 0;
 
 // Right Motor PID Variables
-double kp_right = 10, ki_right = 3, /**/ kd_right = 0, kff_right = 5.25; // Revert kff to 5.5 if doesnt work
+double kp_right = 10, ki_right = 2, /**/ kd_right = 0, kff_right = 5; // Revert kff to 5.5 if doesnt work
 double integral_right = 0, lastError_right = 0;
 
 // Sync PID Variables
